e0ba466fd8a996449555f4e2e350746dc3c91384
Lewis Wright <lewis@vivait.co.uk>
2014-10-15 10:38:03 +0100

    Removed useless method.

--- src/Vivait/TenantBundle/VivaitTenantBundle.php
+++ src/Vivait/TenantBundle/VivaitTenantBundle.php
@@ -7,10 +7,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class VivaitTenantBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-//        $container->addCompilerPass(new VoterCompilerPass());
-    }
 }
