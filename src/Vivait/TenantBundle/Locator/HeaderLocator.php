0abc85d6e1d2befd20fbad61df83371cbf4950c4
Kiel Goodman <kiel@vivait.co.uk>
2015-10-27 14:51:38 +0000

    Added HeaderLocator to identify tenants from a request header parameter

---
+++ src/Vivait/TenantBundle/Locator/HeaderLocator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Vivait\TenantBundle\Locator;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @see HostnameLocatorSpec
+ */
+class HeaderLocator implements TenantLocator
+{
+    private $request;
+
+    /**
+     * @var string
+     */
+    private $tenant_key;
+
+    public function __construct(Request $request, $tenant_key)
+    {
+        $this->request = $request;
+        $this->tenant_key = $tenant_key;
+    }
+
+    /**
+     * @return string Tenant key
+     */
+    public function getTenant()
+    {
+        if (!$this->request->headers->has('tenant')) {
+            throw new \RuntimeException('Could not locate a tenant in the header');
+        }
+
+        return $this->request->headers->get('tenant');
+    }
+
+    /**
+     * @param Request $request
+     * @param string $tenant_key
+     * @return string Tenant key
+     */
+    public static function getTenantFromRequest(Request $request, $tenant_key = 'tenant')
+    {
+        $locator = new self($request, $tenant_key);
+
+        return $locator->getTenant();
+    }
+}
